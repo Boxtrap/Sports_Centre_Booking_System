6bb16cb89d9770df0c8636563d43974ed5a81f42
Scott Graham <sgrahamwork@hotmail.com>
2025-06-09 19:32:42 +0100

    Create Drop_Tables.sql

---
+++ Drop_Tables.sql
@@ -0,0 +1,10 @@
+-- DROP TABLES ----------------------------------------------------------------------------------------------------------------------------------
+DROP TABLE CourtParticipants;
+DROP TABLE CourtBooking;
+DROP TABLE Staff;
+DROP TABLE Court;
+DROP TABLE VitaminPlan;
+DROP TABLE Vitamin;
+DROP TABLE Dosage;
+DROP TABLE Membership;
+DROP TABLE Address;
