27948cd860bb128700b3b2b1bb043bb1b7eae6a2
Scott Graham <sgrahamwork@hotmail.com>
2025-06-09 19:33:34 +0100

    Create drop_sequences.sql

---
+++ drop_sequences.sql
@@ -0,0 +1,9 @@
+-- DROP SEQUENCES -------------------------------------------------------------------------------------------------------------------------------
+DROP SEQUENCE address_sequence; 
+DROP SEQUENCE membership_sequence; 
+DROP SEQUENCE staff_sequence; 
+DROP SEQUENCE court_sequence;
+DROP SEQUENCE court_booking_sequence;
+DROP SEQUENCE dosage_sequence;
+DROP SEQUENCE vitamin_sequence;
+DROP SEQUENCE vitamin_plan_sequence;
