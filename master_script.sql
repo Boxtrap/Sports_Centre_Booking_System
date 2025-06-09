f18ed96b946e15eed2a33be72533f08894117f2e
Scott Graham <sgrahamwork@hotmail.com>
2025-06-09 20:18:53 +0100

    Create master_script.sql

---
+++ master_script.sql
@@ -0,0 +1,11 @@
+@Drop_Statements/drop_tables.sql
+@Drop_Statements/drop_sequences.sql
+
+@Sequences/create_sequences.sql
+
+@Tables/create_tables.sql
+
+@Triggers/court_booking_triggers.sql
+
+@Procedures/create_procedures.sql
+
